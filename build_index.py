36c6ce6c387a4b5e88ef0e729b1d531074aa7070
Romain Xu-Darme <romain.xu@univ-grenoble-alpes.fr>
2021-12-07 16:19:54 +0100

    [TARLOADER] Add build_index.py tool

---
+++ build_index.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+from tarloader import ImageArchive,ImageArchive_add_parser_options
+from argparse import RawTextHelpFormatter
+import argparse
+
+ap = argparse.ArgumentParser(
+	description='Build index for tarloader.',
+	formatter_class=RawTextHelpFormatter)
+ImageArchive_add_parser_options(ap,init=True)
+args = ap.parse_args()
+
+archive = ImageArchive(
+	apath = args.tarloader_archive,
+	ipath = args.tarloader_index_file,
+	root = args.tarloader_archive_root_directory,
+	image_index = args.tarloader_image_file,
+	image_label = args.tarloader_label_file,
+	image_label_preprocessing = None,
+	split_mask = None,
+	split_val = 0,
+	transform = None,
+	index_transform = None,
+	target_transform = None,
+	batch_size = 1,
+	drop_last = False,
+	loader = None,
+	is_valid_file  = None,
+	data_in_memory = False,
+	open_after_fork =  False,
+	overwrite_index = args.tarloader_index_file_overwrite,
+)
+
